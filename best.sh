f5f1b92da2c33bafa104de25d7d527cb7beecdaa
success3854 <105676617+success3854@users.noreply.github.com>
2022-05-20 21:05:36 -0500

    the right path

--- best.sh
+++ best.sh
@@ -1,3 +1,4 @@
 echo " i think devops is the path i want to follow; my professor said 'at least think of the money' "
 The Lord will surely bless the eboule's family through devops career
 when this happen, he will not stop to thank the Lord almighty
+God is so good
