f2fa2e6942f34b3a2b0eedea56404b1e63ac2d0d
success3854 <105676617+success3854@users.noreply.github.com>
2022-05-20 21:04:37 -0500

    updates notes

--- best.sh
+++ best.sh
@@ -1,1 +1,3 @@
 echo " i think devops is the path i want to follow; my professor said 'at least think of the money' "
+The Lord will surely bless the eboule's family through devops career
+when this happen, he will not stop to thank the Lord almighty
